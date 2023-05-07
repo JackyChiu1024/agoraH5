a308d728cd570208af4fb269cbe5f2d6fd27aeb3
JackyChiu <chiu.yen-chieh@a5labs.co>
2023-05-08 02:22:05 +0800

    add cocos package

---
+++ cocos/src/settings.32665.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Lobby.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        resources: "96616",
+        internal: "ee81e",
+        main: "fb437"
+    }
+};
